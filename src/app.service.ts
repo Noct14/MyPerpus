48ae0039201b3a86ba3d2d7024b08a317aeb6e55
Andhika <Dhika.putratama@gmail.com>
2026-02-08 21:39:33 +0700

    feat: integrate Handlebars for view rendering and update app structure and installing Typeorm and mysql dependencies

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHome() {
+    return {
+      title: 'test Halo',
+      message: 'Hello World',
+    };
   }
 }
